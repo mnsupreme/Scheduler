4dd9c30e05d8d0d71fd62ec45e9c85d7bb235629
Mitchell Lee <mnsupreme@gmail.com>
2016-10-22 12:47:35 -0700

    c# stuff

---
+++ CNIT255_Project/CNIT255_Project/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CNIT255_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Hello_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The calculations are complete", "My Application",
+            MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+        }
+    }
+}
