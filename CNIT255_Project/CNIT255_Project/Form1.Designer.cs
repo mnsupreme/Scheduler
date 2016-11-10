419c48833e843a04ec564d5ee6543bfe7bf22214
Areeb96 <areeb.ahsan96@gmail.com>
2016-11-10 08:45:04 -0500

    log in

--- CNIT255_Project/CNIT255_Project/Form1.Designer.cs
+++ CNIT255_Project/CNIT255_Project/Form1.Designer.cs
@@ -34,6 +34,26 @@
             this.label_X = new System.Windows.Forms.Label();
             this.label_Owner = new System.Windows.Forms.Label();
             this.label_Customer = new System.Windows.Forms.Label();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label_Title
@@ -41,9 +61,10 @@
             this.label_Title.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.label_Title.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label_Title.ForeColor = System.Drawing.Color.Black;
-            this.label_Title.Location = new System.Drawing.Point(12, 25);
+            this.label_Title.Location = new System.Drawing.Point(16, 31);
+            this.label_Title.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_Title.Name = "label_Title";
-            this.label_Title.Size = new System.Drawing.Size(974, 38);
+            this.label_Title.Size = new System.Drawing.Size(1299, 47);
             this.label_Title.TabIndex = 0;
             this.label_Title.Text = "Starving Boiler";
             // 
@@ -53,23 +74,25 @@
             this.listBox_Rest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.listBox_Rest.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox_Rest.FormattingEnabled = true;
-            this.listBox_Rest.ItemHeight = 25;
+            this.listBox_Rest.ItemHeight = 31;
             this.listBox_Rest.Items.AddRange(new object[] {
             "Rest_1",
             "Rest_2",
             "..."});
-            this.listBox_Rest.Location = new System.Drawing.Point(19, 91);
+            this.listBox_Rest.Location = new System.Drawing.Point(25, 112);
+            this.listBox_Rest.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox_Rest.Name = "listBox_Rest";
-            this.listBox_Rest.Size = new System.Drawing.Size(238, 177);
+            this.listBox_Rest.Size = new System.Drawing.Size(317, 188);
             this.listBox_Rest.TabIndex = 1;
             // 
             // Label_frm
             // 
             this.Label_frm.BackColor = System.Drawing.SystemColors.Control;
             this.Label_frm.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.Label_frm.Location = new System.Drawing.Point(853, 2);
+            this.Label_frm.Location = new System.Drawing.Point(1137, 2);
+            this.Label_frm.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Label_frm.Name = "Label_frm";
-            this.Label_frm.Size = new System.Drawing.Size(100, 23);
+            this.Label_frm.Size = new System.Drawing.Size(133, 28);
             this.Label_frm.TabIndex = 2;
             this.Label_frm.Text = "     ";
             // 
@@ -79,9 +102,10 @@
             this.label_X.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label_X.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label_X.ForeColor = System.Drawing.Color.Black;
-            this.label_X.Location = new System.Drawing.Point(853, 43);
+            this.label_X.Location = new System.Drawing.Point(1137, 53);
+            this.label_X.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_X.Name = "label_X";
-            this.label_X.Size = new System.Drawing.Size(37, 39);
+            this.label_X.Size = new System.Drawing.Size(49, 48);
             this.label_X.TabIndex = 3;
             this.label_X.Text = "X";
             this.label_X.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -93,9 +117,10 @@
             this.label_Owner.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label_Owner.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label_Owner.ForeColor = System.Drawing.Color.Black;
-            this.label_Owner.Location = new System.Drawing.Point(853, 161);
+            this.label_Owner.Location = new System.Drawing.Point(1137, 198);
+            this.label_Owner.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_Owner.Name = "label_Owner";
-            this.label_Owner.Size = new System.Drawing.Size(131, 41);
+            this.label_Owner.Size = new System.Drawing.Size(174, 50);
             this.label_Owner.TabIndex = 4;
             this.label_Owner.Text = "Owner";
             this.label_Owner.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -106,19 +131,228 @@
             this.label_Customer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label_Customer.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label_Customer.ForeColor = System.Drawing.Color.Black;
-            this.label_Customer.Location = new System.Drawing.Point(853, 98);
+            this.label_Customer.Location = new System.Drawing.Point(1137, 121);
+            this.label_Customer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_Customer.Name = "label_Customer";
-            this.label_Customer.Size = new System.Drawing.Size(168, 41);
+            this.label_Customer.Size = new System.Drawing.Size(223, 50);
             this.label_Customer.TabIndex = 5;
             this.label_Customer.Text = "Customer";
             this.label_Customer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(518, 217);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(68, 21);
+            this.checkBox9.TabIndex = 64;
+            this.checkBox9.Text = "Indian";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(631, 161);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(67, 21);
+            this.checkBox8.TabIndex = 63;
+            this.checkBox8.Text = "Italian";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(518, 188);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(71, 21);
+            this.checkBox7.TabIndex = 62;
+            this.checkBox7.Text = "Coffee";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(631, 188);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(92, 21);
+            this.checkBox6.TabIndex = 61;
+            this.checkBox6.Text = "Japanese";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(631, 218);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(108, 21);
+            this.checkBox5.TabIndex = 60;
+            this.checkBox5.Text = "International";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(518, 161);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(90, 21);
+            this.checkBox3.TabIndex = 59;
+            this.checkBox3.Text = "Breakfast";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(407, 257);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 58;
+            this.label1.Text = "Suggestions:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(409, 277);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(426, 132);
+            this.listBox2.TabIndex = 57;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(870, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 51);
+            this.button1.TabIndex = 56;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(409, 217);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 21);
+            this.checkBox4.TabIndex = 55;
+            this.checkBox4.Text = "Burgers";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(410, 188);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(81, 21);
+            this.checkBox2.TabIndex = 54;
+            this.checkBox2.Text = "Mexican";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(410, 161);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(81, 21);
+            this.checkBox1.TabIndex = 53;
+            this.checkBox1.Text = "Chinese";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(870, 432);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 51);
+            this.button4.TabIndex = 52;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(870, 375);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(103, 51);
+            this.button5.TabIndex = 51;
+            this.button5.Text = "Add Restaurant";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(870, 318);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(103, 51);
+            this.button6.TabIndex = 50;
+            this.button6.Text = "Clear";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(870, 259);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(103, 51);
+            this.button7.TabIndex = 49;
+            this.button7.Text = "See Menu(s)";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(410, 432);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(426, 132);
+            this.listBox1.TabIndex = 48;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(407, 412);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 17);
+            this.label7.TabIndex = 47;
+            this.label7.Text = "Menu:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(605, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 17);
+            this.label6.TabIndex = 46;
+            this.label6.Text = "255 Project";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(554, 293);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 17);
+            this.label9.TabIndex = 45;
+            // 
             // StarvingBoiler_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Orange;
-            this.ClientSize = new System.Drawing.Size(1035, 531);
+            this.ClientSize = new System.Drawing.Size(1380, 654);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label_Customer);
             this.Controls.Add(this.label_Owner);
             this.Controls.Add(this.label_X);
@@ -126,12 +360,14 @@
             this.Controls.Add(this.label_Title);
             this.Controls.Add(this.Label_frm);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "StarvingBoiler_Form";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "StarvingBoiler";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -143,6 +379,26 @@
         private System.Windows.Forms.Label label_X;
         private System.Windows.Forms.Label label_Owner;
         private System.Windows.Forms.Label label_Customer;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
     }
 }
 
---
+++ CNIT255_Project/CNIT255_Project/Form2.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace CNIT255_Project
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.Login = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(296, 171);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(76, 171);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(75, 23);
+            this.Login.TabIndex = 19;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(165, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(206, 22);
+            this.textBox2.TabIndex = 17;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 22);
+            this.textBox1.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Username:";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 248);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
